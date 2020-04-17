329f3f01ebceae8b773dfd9da8995e2ebd04b116
笠浪貴大 <dev.kacha@gmail.com>
2020-04-18 00:39:57 +0900

    EncodingをASCIIに変更

--- FileExtensionChanger/FileExtensionChanger/Checker/JpegChecker.cs
+++ FileExtensionChanger/FileExtensionChanger/Checker/JpegChecker.cs
@@ -7,8 +7,14 @@ namespace FileExtensionChanger.Checker
 {
     public class JpegChecker
     {
-        static readonly byte[] JFIF = Encoding.UTF8.GetBytes("JFIF\0");
-        static readonly byte[] ICC_PROFILE = Encoding.UTF8.GetBytes("ICC_PROFILE\0");
+        /// <summary>
+        /// ASCII文字で"JFIF"とヌル終端
+        /// </summary>
+        static readonly byte[] JFIF = Encoding.ASCII.GetBytes("JFIF\0");
+        /// <summary>
+        /// ASCII文字で"ICC_PROFILE"とヌル終端
+        /// </summary>
+        static readonly byte[] ICC_PROFILE = Encoding.ASCII.GetBytes("ICC_PROFILE\0");
         /// <summary>
         /// JPEGファイルならtrueを返す。
         /// </summary>
@@ -27,14 +33,12 @@ namespace FileExtensionChanger.Checker
             // JFIFフォーマット APP0 (0xFFE0)
             if (buffer[2] == 0xFF && buffer[3] == 0xE0)
             {
-                // ASCII文字で"JFIF"とヌル終端
                 var slicedBuffer = buffer.Slice(6, JFIF.Length);
                 return slicedBuffer.SequenceEqual(JFIF);
             }
             // カラープロファイル APP2 (0xFFE2)
             if (buffer[2] == 0xFF && buffer[3] == 0xE2)
             {
-                // ASCII文字で"ICC_PROFILE"とヌル終端
                 var slicedBuffer = buffer.Slice(6, ICC_PROFILE.Length);
                 return slicedBuffer.SequenceEqual(ICC_PROFILE);
             }
